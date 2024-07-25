354a5126154bd13df0e10988cf3434cb4f8f2232
krupal vasa <krupalbtech@gmail.com>
2024-07-25 09:40:12 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+	access_key = ""
+	secret_key = ""
+	region = ""
+}
 
+resource "aws_instance" "benny_instance" {
+	ami = ""
+	instance_type = "t2.micro"
+	availability_zone = ""
+	key_name = ""
+	vpc_security_group_ids = ""
+	tags = {
+	  Name = ""
+	  project = ""
+	}
+}
